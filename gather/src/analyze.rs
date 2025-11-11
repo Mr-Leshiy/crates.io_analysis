31f36f51f5b86da6c826d5788704fe95f2e6ad71
Mr-Leshiy <leshiy12345678@gmail.com>
2025-11-11 22:50:04 +0700

    add cargo deny analyzing

--- gather/src/analyze.rs
+++ gather/src/analyze.rs
@@ -1,4 +1,4 @@
-use std::{collections::HashMap, path::Path};
+use std::{collections::HashMap, path::Path, sync::LazyLock};
 
 use cargo::{
     GlobalContext,
@@ -10,6 +10,13 @@ use cargo::{
     },
     ops::resolve_ws_with_opts,
 };
+use cargo_deny::{
+    CheckCtx, Spanned, UnvalidatedConfig,
+    advisories::{self, cfg::Config},
+    diag::{ErrorSink, Files, KrateSpans},
+    utf8path,
+};
+use krates::{NoneFilter, Utf8PathBuf};
 
 #[derive(Debug, Clone)]
 pub struct CrateInfo {
@@ -32,12 +39,12 @@ pub async fn analyze(crate_dir: &Path) -> anyhow::Result<CrateInfo> {
     let ws = Workspace::new(&crate_dir.join("Cargo.toml"), &ctx)?;
 
     let (direct_deps, all_deps) = get_crate_deps_count(&ws)?;
+    cargo_deny_check(&ws)?;
 
     let info = CrateInfo {
         direct_deps,
         all_deps,
     };
-
     tracing::info!(crate_dir = ?crate_dir, info = ?info, "Crate analyzed.");
 
     Ok(info)
@@ -85,4 +92,95 @@ fn get_crate_deps_count(ws: &Workspace) -> anyhow::Result<(usize, usize)> {
     let all_deps = package_map.len();
 
     Ok((direct_deps, all_deps))
+}
+
+const ADVISORY_DB_PATH: LazyLock<anyhow::Result<Utf8PathBuf>> =
+    LazyLock::new(|| utf8path(home::cargo_home()?.join("advisory-dbs")));
+
+static ADVISORY_DB: LazyLock<anyhow::Result<advisories::DbSet>> = LazyLock::new(|| {
+    let db_path = ADVISORY_DB_PATH
+        .as_ref()
+        .map_err(|e| anyhow::anyhow!("advisory db path must be initialized: {e}"))?
+        .clone();
+    advisories::DbSet::load(db_path, vec![], advisories::Fetch::Allow)
+});
+
+fn cargo_deny_check(ws: &Workspace) -> anyhow::Result<()> {
+    let mut files = Files::new();
+    // write down default `deny.default.toml`
+    let cfg_id = files.add(
+        &cargo_deny::PathBuf::from("deny.default.toml"),
+        String::new(),
+    );
+
+    let cfg = {
+        let mut cfg = Config::default();
+        cfg.yanked = Spanned::new(cargo_deny::LintLevel::Deny);
+        cfg.db_path = Some(Spanned::new(
+            ADVISORY_DB_PATH
+                .as_ref()
+                .map_err(|e| anyhow::anyhow!("advisory db path must be initialized: {e}"))?
+                .clone(),
+        ));
+        cfg.validate(cargo_deny::cfg::ValidationContext {
+            cfg_id,
+            files: &mut files,
+            diagnostics: &mut vec![],
+        })
+    };
+
+    let advisory_db_set = ADVISORY_DB
+        .as_ref()
+        .map_err(|e| anyhow::anyhow!("advisory db initialized: {e}"))?;
+
+    let krates = {
+        let options = cargo::ops::OutputMetadataOptions {
+            cli_features: CliFeatures::new_all(true),
+            no_deps: false,
+            version: 1,
+            filter_platforms: vec![],
+        };
+        let md = cargo::ops::output_metadata(&ws, &options)?;
+        let md = serde_json::from_value::<krates::cm::Metadata>(serde_json::to_value(md)?)?;
+
+        let mut kb = krates::Builder::new();
+        kb.ignore_kind(krates::DepKind::Dev, krates::Scope::All);
+        kb.build_with_metadata(md, NoneFilter)?
+    };
+    let indices = Some(advisories::Indices::load(
+        &krates,
+        utf8path(home::cargo_home()?)?,
+    ));
+    let krate_spans =
+        &KrateSpans::synthesize(&krates, krates.workspace_root().as_str(), &mut files);
+    let ctx = CheckCtx {
+        cfg,
+        krates: &krates,
+        krate_spans,
+        colorize: false,
+        serialize_extra: false,
+        log_level: log::LevelFilter::Off,
+        files: &files,
+    };
+    let audit_reporter = Some(|_| {});
+
+    let (tx, rx) = crossbeam_channel::unbounded();
+    let advisories_sink = ErrorSink {
+        overrides: None,
+        channel: tx,
+    };
+
+    advisories::check(
+        ctx,
+        &advisory_db_set,
+        audit_reporter,
+        indices,
+        advisories_sink,
+    );
+
+    for d in rx.iter().flat_map(|p| p.into_iter()) {
+        tracing::info!(code = ?d.code, message = d.diag.message);
+    }
+
+    Ok(())
 }
